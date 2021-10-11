9716940953ef44d2b2676c63911c6c11e1dfcfd3
1389fuzita <toto13.atsu0809@icloud.com>
2021-10-11 14:07:30 +0000

    [Finish 3-5]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html resources :blogs
+  resources :blogs
+  post 'blogs/index' => 'blogs#index'
+  post 'blogs/show' => 'blogs#show'
+  post 'blogs/new' => 'blogs#new'
+  post 'blogs/edit' => 'blogs#edit'
 end

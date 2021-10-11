1186320287a3744854f736632044541587581f7e
1389fuzita <toto13.atsu0809@icloud.com>
2021-10-11 23:32:36 +0000

    [Finish 3-8]

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,10 +13,14 @@ class BlogsController < ApplicationController
   end
   
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path(blog.id)
+     @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog.id)
+    else
+      render :new
+    end
   end
+  
 
   def edit
     @blog = Blog.find(params[:id])
@@ -32,7 +36,7 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.destroy
     redirect_to blogs_path
-end
+  end
   
   private
   def blog_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html resources :blogs
   resources :blogs
-  post 'blogs/index' => 'blogs#index'
-  post 'blogs/show' => 'blogs#show'
-  post 'blogs/new' => 'blogs#new'
-  post 'blogs/edit' => 'blogs#edit'
+  
 end
